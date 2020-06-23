527316a9f24689219ab70bfb45fdeb98764e1d40
LittleBigProgramming <20643587+LittleBigProgramming@users.noreply.github.com>
2020-06-23 21:27:27 +0100

    Installed tailwind, added to laravel mix webpack configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <link rel="stylesheet" href="/css/main.css">
+
         <!-- Styles -->
         <style>
             html, body {
