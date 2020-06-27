976954332cedd80b9cd55e478514bdadb7958739
LittleBigProgramming <20643587+LittleBigProgramming@users.noreply.github.com>
2020-06-27 22:10:43 +0100

    Added app layout and index styles containing grid items

---
+++ resources/views/index.blade.php
@@ -0,0 +1,225 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mx-auto px-4">
+        <h2 class="text-blue-400 uppercase tracking-wide font-semibold">Popular Items</h2>
+        <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-700 pb-8 pt-4">
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/500x750" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-700 rounded-full" style="right:-20px; bottom: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Item Title
+                </a>
+                <div class="text-gray-400 mt-1">
+                    Category
+                </div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/500x750" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-700 rounded-full" style="right:-20px; bottom: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Item Title
+                </a>
+                <div class="text-gray-400 mt-1">
+                    Category
+                </div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/500x750" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-700 rounded-full" style="right:-20px; bottom: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Item Title
+                </a>
+                <div class="text-gray-400 mt-1">
+                    Category
+                </div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/500x750" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-700 rounded-full" style="right:-20px; bottom: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Item Title
+                </a>
+                <div class="text-gray-400 mt-1">
+                    Category
+                </div>
+            </div>
+            <div class="game mt-4">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/500x750" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-700 rounded-full" style="right:-20px; bottom: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Item Title
+                </a>
+                <div class="text-gray-400 mt-1">
+                    Category
+                </div>
+            </div>
+            <div class="game mt-4">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/500x750" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-700 rounded-full" style="right:-20px; bottom: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Item Title
+                </a>
+                <div class="text-gray-400 mt-1">
+                    Category
+                </div>
+            </div>
+            <div class="game mt-4">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/500x750" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-700 rounded-full" style="right:-20px; bottom: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Item Title
+                </a>
+                <div class="text-gray-400 mt-1">
+                    Category
+                </div>
+            </div>
+            <div class="game mt-4">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/500x750" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-700 rounded-full" style="right:-20px; bottom: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Item Title
+                </a>
+                <div class="text-gray-400 mt-1">
+                    Category
+                </div>
+            </div>
+            <div class="game mt-4">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/500x750" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-700 rounded-full" style="right:-20px; bottom: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Item Title
+                </a>
+                <div class="text-gray-400 mt-1">
+                    Category
+                </div>
+            </div>
+            <div class="game mt-4">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/500x750" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-700 rounded-full" style="right:-20px; bottom: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Item Title
+                </a>
+                <div class="text-gray-400 mt-1">
+                    Category
+                </div>
+            </div>
+            <div class="game mt-4">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/500x750" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-700 rounded-full" style="right:-20px; bottom: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Item Title
+                </a>
+                <div class="text-gray-400 mt-1">
+                    Category
+                </div>
+            </div>
+            <div class="game mt-4">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/500x750" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-700 rounded-full" style="right:-20px; bottom: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Item Title
+                </a>
+                <div class="text-gray-400 mt-1">
+                    Category
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
