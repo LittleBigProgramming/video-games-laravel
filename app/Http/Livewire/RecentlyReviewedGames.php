cb15cd4e00f1310dcca4bd7a0fa735003d0c6b7a
LittleBigProgramming <20643587+LittleBigProgramming@users.noreply.github.com>
2020-10-31 18:04:23 +0000

    Retrieve and store the api data for 300 seconds (5 minutes) within the Cache

--- app/Http/Livewire/RecentlyReviewedGames.php
+++ app/Http/Livewire/RecentlyReviewedGames.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
@@ -15,9 +16,10 @@ class RecentlyReviewedGames extends Component
         $before = Carbon::now()->subMonths(2)->timestamp;
         $current = Carbon::now()->timestamp;
 
-        $this->recentlyReviewedGames = Http::withHeaders(config('services.igdb.headers'))
-            ->withBody(
-                "fields name, cover.url, first_release_date, total_rating_count, platforms.abbreviation, rating, rating_count, summary, slug;
+        $this->recentlyReviewedGames = Cache::remember('recently-reviewed-games', 300, function () use($before, $current) {
+            return Http::withHeaders(config('services.igdb.headers'))
+                ->withBody(
+                    "fields name, cover.url, first_release_date, total_rating_count, platforms.abbreviation, rating, rating_count, summary, slug;
                     where platforms = (48,49,130,6)
                     & (first_release_date >= {$before}
                     & first_release_date < {$current}
@@ -25,8 +27,9 @@ class RecentlyReviewedGames extends Component
                     sort total_rating_count desc;
                     limit 3;
                 ", "text/plain"
-            )->post(config('services.igdb.games-endpoint'))
-            ->json();
+                )->post(config('services.igdb.games-endpoint'))
+                ->json();
+        });
     }
 
     public function render()
--- resources/views/livewire/coming-soon-games.blade.php
+++ resources/views/livewire/coming-soon-games.blade.php
@@ -1,9 +1,11 @@
 <div wire:init="loadComingSoonGames" class="most-anticipated-container space-y-10 mt-8 mb-8">
     @forelse ($comingSoonGames as $game)
         <div class="item flex">
-            <a href="#">
-                <img src="{{ Str::replaceFirst('thumb', 'cover_small', $game['cover']['url']) }}" alt="" class="w-24 hover:opacity-75 transition ease-in-out duration-150">
-            </a>
+            @if (isset($game['cover']))
+                <a href="#">
+                    <img src="{{ Str::replaceFirst('thumb', 'cover_small', $game['cover']['url']) }}" alt="" class="w-24 hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+            @endif
             <div class="ml-4">
                 <a href="#" class="hover:text-gray-300">{{ $game['name'] }}</a>
                 <div class="text-gray-400 text-sm mt-1">
