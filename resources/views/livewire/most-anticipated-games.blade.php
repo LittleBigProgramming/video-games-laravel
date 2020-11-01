ec863a64468c2572d901598f640810251642d3ab
LittleBigProgramming <20643587+LittleBigProgramming@users.noreply.github.com>
2020-11-01 12:23:43 +0000

    Added skeleton loader for livewire components making AJAX calls

--- resources/views/livewire/most-anticipated-games.blade.php
+++ resources/views/livewire/most-anticipated-games.blade.php
@@ -14,6 +14,16 @@
             </div>
         </div>
     @empty
-        <div class="spinner my-8 p-6"></div>
+        @foreach(range(1, 4) as $game)
+            <div class="game flex">
+                <div class="bg-gray-700 w-16 h-20 flex-none">
+                    <div class="spinner my-8 p-6"></div>
+                </div>
+                <div class="ml-4">
+                    <div class="text-transparent bg-gray-700 leading-tight">Title goes here today</div>
+                    <div class="text-transparent bg-gray-700 inline-block text-sm mt-2">Sept 14, 2020</div>
+                </div>
+            </div>
+        @endforeach
     @endforelse
 </div>
